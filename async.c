72c6cd37cbc46c25524a97c9825e6ea4694237da
Kazami <kazamizero@gmail.com>
2016-04-20 01:23:04 +0800

    title: async edited what: change to lock free pool

--- async.c
+++ async.c
@@ -6,6 +6,39 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+
+enum {
+    ASYNC_ERROR,
+    ASYNC_WARNING,
+    ASYNC_DEBUG
+};
+
+#define debug(level, ...) do { \
+    if (level < ASYNC_DEBUG) { \
+        flockfile(stdout); \
+        printf("###%p.%s: ", (void *)pthread_self(), __func__); \
+        printf(__VA_ARGS__); \
+        putchar('\n'); \
+        fflush(stdout); \
+        funlockfile(stdout); \
+    } \
+} while (0)
+
+#define WORK_QUEUE_POWER 16 
+#define JOB_QUEUE_SIZE (1 << WORK_QUEUE_POWER)
+#define JOB_QUEUE_MASK (JOB_QUEUE_SIZE - 1)
+
+#define queue_out_val(thread) (__sync_val_compare_and_swap(&(thread)->out, 0, 0))
+#define queue_len(thread) ((thread)->in - queue_out_val(thread))
+#define queue_empty(thread) (queue_len(thread) == 0)
+#define queue_full(thread) (queue_len(thread) == JOB_QUEUE_SIZE)
+#define job_offset(val) ((val) & JOB_QUEUE_MASK)
+
+static volatile int global_num_threads = 0;
+static pthread_t main_thread;
 
 /* suppress compilation warnings */
 static inline ssize_t write_wrapper(int fd, const void *buf, size_t count)
@@ -20,8 +53,6 @@ static inline ssize_t write_wrapper(int fd, const void *buf, size_t count)
 /* the actual working thread */
 static void *worker_thread_cycle(void *async);
 
-/* signaling to finish */
-static void async_signal(async_p async);
 
 /* the destructor */
 static void async_destroy(async_p queue);
@@ -33,12 +64,34 @@ static void *join_thread(pthread_t thr)
     return ret;
 }
 
-static int create_thread(pthread_t *thr,
-                         void *(*thread_func)(void *),
-                         void *async)
-{
-    return pthread_create(thr, NULL, thread_func, async);
+
+static int wait_for_registration(int expected) {
+    sigset_t zeromask, newmask, oldmask;
+        //debug(ASYNC_ERROR, "WAITING");
+
+    sigemptyset(&zeromask);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGUSR1);
+
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0) {
+        debug(ASYNC_ERROR, "SIG_BLOCK_FAILED");
+        return -1;
+    }
+
+        //debug(ASYNC_ERROR, "GO TO SLEEP");
+    while (global_num_threads < expected) {
+        //debug(ASYNC_ERROR, "SLEEP");
+        sigsuspend(&zeromask);
+    }
+        //debug(ASYNC_ERROR, "STOP SLEEP");
+
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0) {
+        return -1;
+    }
+
+    return 0;
 }
+
 
 /** A task node */
 struct AsyncTask {
@@ -47,144 +100,194 @@ struct AsyncTask {
     void *arg;
 };
 
+typedef struct {
+    pthread_t       id;
+    unsigned int    in;
+    unsigned int    out;
+    unsigned run : 1; /**< the running flag */
+    struct AsyncTask       job_queue[JOB_QUEUE_SIZE];
+} thread_u;
+
+typedef thread_u *(*schedule_func)(struct Async *);
 /** The Async struct */
 struct Async {
     /** the task queue - MUST be first in the struct */
-    pthread_mutex_t lock;              /**< a mutex for data integrity */
-    struct AsyncTask * volatile tasks;  /**< active tasks */
-    struct AsyncTask * volatile pool;   /**< a task node pool */
-    struct AsyncTask ** volatile pos;   /**< the position for new tasks */
-
-    /** The pipe used for thread wakeup */
-    struct {
-        int in;  /**< read incoming data (opaque data), used for wakeup */
-        int out; /**< write opaque data (single byte),
-                      used for wakeup signaling */
-    } pipe;
 
     int count; /**< the number of initialized threads */
 
-    unsigned run : 1; /**< the running flag */
-
+    schedule_func schedule_thread;
+    
     /** the thread pool */
-    pthread_t threads[];
+    thread_u threads[];
 };
+
+static int create_threads(async_p async, int index)
+{
+    memset(&async->threads[index], 0, sizeof(thread_u));
+    return pthread_create(&async->threads[index].id, NULL, worker_thread_cycle, (void *)(&async->threads[index]));
+}
+
+static thread_u *round_robin_schedule(async_p async)
+{
+    static int cur_thr_index = -1;
+
+    cur_thr_index = (cur_thr_index + 1) % async->count;
+
+    return &async->threads[cur_thr_index];
+}
+
+static int dispatch_2thread(async_p async, thread_u *thread, void (*task)(void *), void *arg) {
+    struct AsyncTask *job = NULL;
+
+    job = &thread->job_queue[job_offset(thread->in)];
+    job->task = task;
+    job->next = NULL;
+    job->arg = arg;
+    thread->in++;
+    //debug(ASYNC_ERROR, "START dispatch");
+    if (queue_len(thread) >= 0) {
+        pthread_kill(thread->id, SIGUSR1);
+    }
+
+    return 0;
+}
+
+struct AsyncTask *get_workcurrently(thread_u *thread) 
+{
+    struct AsyncTask *job = NULL;
+    unsigned int tmp;
+    do {
+
+        if (queue_len(thread) <= 0) {
+            //debug(ASYNC_ERROR, "thread set");
+            break;
+        }
+
+        tmp = thread->out;
+   
+        job = &thread->job_queue[job_offset(tmp)];
+    } while (!__sync_bool_compare_and_swap(&thread->out, tmp, tmp + 1));
+    //debug(ASYNC_ERROR, "GET WORK");
+        return job;
+}
 
 /* Task Management - add a task and perform all tasks in queue */
 
 static int async_run(async_p async, void (*task)(void *), void *arg)
 {
-    struct AsyncTask *c;  /* the container, storing the task */
-
-    if (!async || !task) return -1;
-
-    pthread_mutex_lock(&(async->lock));
-    /* get a container from the pool of grab a new container */
-    if (async->pool) {
-        c = async->pool;
-        async->pool = async->pool->next;
-    } else {
-        c = malloc(sizeof(*c));
-        if (!c) {
-            pthread_mutex_unlock(&async->lock);
-            return -1;
-        }
-    }
-    c->next = NULL;
-    c->task = task;
-    c->arg = arg;
-    if (async->tasks) {
-        *(async->pos) = c;
-    } else {
-        async->tasks = c;
-    }
-    async->pos = &(c->next);
-    pthread_mutex_unlock(&async->lock);
-    /* wake up any sleeping threads
-     * any activated threads will ask to require the mutex
-     * as soon as we write.
-     * we need to unlock before we write, or we will have excess
-     * context switches.
-     */
-    write(async->pipe.out, c->task, 1);
-    return 0;
+    thread_u *thread;
+    thread = async->schedule_thread(async);
+    //debug(ASYNC_ERROR, "START SCHEDULE");
+    return dispatch_2thread(async, thread, task, arg);
 }
 
 /** Performs all the existing tasks in the queue. */
-static void perform_tasks(async_p async)
+static void perform_tasks(thread_u *thread)
 {
     struct AsyncTask *c = NULL;  /* c == container, will store the task */
-    do {
-        /* grab a task from the queue. */
-        pthread_mutex_lock(&(async->lock));
-        /* move the old task container to the pool. */
-        if (c) {
-            c->next = async->pool;
-            async->pool = c;
-        }
-        c = async->tasks;
-        if (c) {
-            /* move the queue forward. */
-            async->tasks = async->tasks->next;
-        }
-        pthread_mutex_unlock(&(async->lock));
-        /* perform the task */
-        if (c) c->task(c->arg);
-    } while (c);
+
+    c = get_workcurrently(thread);
+    //debug(ASYNC_ERROR, "get work");
+
+    if (c) {
+    //debug(ASYNC_ERROR, "perform work");
+        (*(c->task))(c->arg);
+    }
 }
 
 /* The worker threads */
 
 /* The worker cycle */
-static void *worker_thread_cycle(void *_async)
+static void *worker_thread_cycle(void *_thread)
 {
     /* setup signal and thread's local-storage async variable. */
-    struct Async *async = _async;
-    char sig_buf;
+    thread_u *thread = _thread;
+    thread->run = 1;
+    sigset_t zeromask, oldmask, newmask;
 
-    /* pause for signal for as long as we're active. */
-    while (async->run && (read(async->pipe.in, &sig_buf, 1) >= 0)) {
-        perform_tasks(async);
-        sched_yield();
+    __sync_fetch_and_add(&global_num_threads, 1);
+    pthread_kill(main_thread, SIGUSR1);
+    sigemptyset(&zeromask);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGUSR1);
+
+    //debug(ASYNC_ERROR, "worker_thread");
+    while (1) {
+    //debug(ASYNC_ERROR, "running");
+
+        if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0) {
+            perror("sig failed");
+            pthread_exit(NULL);
+        }
+
+        while (queue_empty(thread) && thread->run) {
+    //debug(ASYNC_ERROR, "sleep");
+            sigsuspend(&zeromask);
+        }
+
+        if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0) {
+            debug(ASYNC_ERROR, "set mask failed");
+            pthread_exit(NULL);
+        }
+
+        if (thread->run == 0) {
+    debug(ASYNC_ERROR, "dead");
+            pthread_exit(NULL);
+        }
+
+        perform_tasks(thread);
+
+        if (queue_empty(thread)) {
+            pthread_kill(main_thread, SIGUSR1);
+        }
     }
 
-    perform_tasks(async);
     return 0;
+}
+
+void sig_do_nothing()
+{
+    return;
+}
+
+static int async_queue_empty(async_p async)
+{
+    int i;
+    for (i = 0; i < async->count; i++) {
+        if (!queue_empty(&async->threads[i]))
+            return 0;
+    }
+    return 1;
 }
 
 /* Signal and finish */
 
-static void async_signal(async_p async)
-{
-    async->run = 0;
-    /* send `async->count` number of wakeup signales.
-     * data content is irrelevant. */
-    write(async->pipe.out, async, async->count);
-}
 
 static void async_wait(async_p async)
 {
-    if (!async) return;
+    sigset_t zeromask, oldmask, newmask;
+    sigemptyset(&zeromask);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGUSR1);
 
-    /* wake threads (just in case) by sending `async->count`
-     * number of wakeups
-     */
-    if (async->pipe.out)
-        write(async->pipe.out, async, async->count);
-    /* join threads */
-    for (int i = 0; i < async->count; i++) {
-        join_thread(async->threads[i]);
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask)) {
+            debug(ASYNC_ERROR, "SIG BLOCK failed");
+            pthread_exit(NULL);
     }
-    /* perform any pending tasks */
-    perform_tasks(async);
-    /* release queue memory and resources */
-    async_destroy(async);
+
+    while (!async_queue_empty(async))
+        sigsuspend(&zeromask);
+
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0) {
+        debug(ASYNC_ERROR, "SIG_SETMASK failed");
+        pthread_exit(NULL);
+    }
 }
 
 static void async_finish(async_p async)
 {
-    async_signal(async);
     async_wait(async);
+    async_destroy(async);
 }
 
 /* Object creation and destruction */
@@ -192,73 +295,50 @@ static void async_finish(async_p async)
 /** Destroys the Async object, releasing its memory. */
 static void async_destroy(async_p async)
 {
-    pthread_mutex_lock(&async->lock);
-    struct AsyncTask *to_free;
-    async->pos = NULL;
-    /* free all tasks */
-    struct AsyncTask *pos = async->tasks;
-    while ((to_free = pos)) {
-        pos = pos->next;
-        free(to_free);
+    int i;
+    for (i = 0; i < async->count; i++) {
+        async->threads[i].run = 0;
+
+        pthread_kill(async->threads[i].id, SIGUSR1);
     }
-    async->tasks = NULL;
-    /* free task pool */
-    pos = async->pool;
-    while ((to_free = pos)) {
-        pos = pos->next;
-        free(to_free);
-    }
-    async->pool = NULL;
-    /* close pipe */
-    if (async->pipe.in) {
-        close(async->pipe.in);
-        async->pipe.in = 0;
-    }
-    if (async->pipe.out) {
-        close(async->pipe.out);
-        async->pipe.out = 0;
-    }
-    pthread_mutex_unlock(&async->lock);
-    pthread_mutex_destroy(&async->lock);
+
+    for (i = 0; i < async->count; i++)
+        join_thread(async->threads[i].id);
+
     free(async);
 }
 
 static async_p async_create(int threads)
 {
-    async_p async = malloc(sizeof(*async) + (threads * sizeof(pthread_t)));
-    async->tasks = NULL;
-    async->pool = NULL;
-    async->pipe.in = 0;
-    async->pipe.out = 0;
-    if (pthread_mutex_init(&(async->lock), NULL)) {
-        free(async);
-        return NULL;
-    };
-    if (pipe((int *) &(async->pipe))) {
-        free(async);
-        return NULL;
-    };
-    fcntl(async->pipe.out, F_SETFL, O_NONBLOCK | O_WRONLY);
-    async->run = 1;
+    int i;
+    async_p async = malloc(sizeof(*async) + (threads * sizeof(thread_u)));
+
+    memset(async, 0, sizeof(*async) + (threads * sizeof(thread_u)));
+    async->count = threads;
+    debug(ASYNC_ERROR, "new in");
+    async->schedule_thread = round_robin_schedule;
     /* create threads */
-    for (async->count = 0; async->count < threads; async->count++) {
-        if (create_thread(async->threads + async->count,
-                          worker_thread_cycle, async)) {
-            /* signal */
-            async_signal(async);
-            /* wait for threads and destroy object */
-            async_wait(async);
-            /* return error */
-            return NULL;
-        };
+    if (signal(SIGUSR1, sig_do_nothing) == SIG_ERR) {
+        return NULL;
     }
+
+    main_thread = pthread_self();
+
+    for (i = 0; i < threads; i++) {
+        create_threads(async, i);
+    debug(ASYNC_ERROR, "threads");
+    }
+    debug(ASYNC_ERROR, "threads");
+    if (wait_for_registration(threads) < 0)
+        pthread_exit(NULL);
+
+    debug(ASYNC_ERROR, "threads");
     return async;
 }
 
 /* API gateway */
 struct __ASYNC_API__ Async = {
     .create = async_create,
-    .signal = async_signal,
     .wait = async_wait,
     .finish = async_finish,
     .run = async_run,
